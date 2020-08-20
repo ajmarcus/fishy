ea81e248d2b125360d64eb0aa696f6345668127f
Ariel Marcus <hi@arielmarcus.com>
2020-08-20 14:25:08 -0400

    add load test

--- app.py
+++ app.py
@@ -5,11 +5,17 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 db = create_engine(getenv('DB'))
+load = getenv('LOAD')
 
 
 @app.route('/health')
 def health():
     return 'healthy!'
+
+
+@app.route('/{load}.txt'.format(load=load))
+def load_test():
+    return load
 
 
 @app.route('/')
